db2203f9b3c463c4690360eb47d6c33b9cbb4d93
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-31 11:01:48 -0300

    initial commit

---
+++ src/main/java/pedroleonez/mais_barato/MaisBaratoApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.mais_barato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisBaratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaisBaratoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/mais_barato/MaisBaratoApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.mais_barato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisBaratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
