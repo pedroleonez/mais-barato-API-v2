1d0d3a47c0275a848b9e4fe22a370afd623ec02e
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-03-31 14:59:32 -0300

    feat: create and impl ProductDTO

---
+++ src/main/java/pedroleonez/mais_barato/dtos/product/ProductDTO.java
@@ -0,0 +1,15 @@
+package pedroleonez.mais_barato.dtos.product;
+
+import java.math.BigDecimal;
+
+public record ProductDTO(
+        Long id,
+        String name,
+        BigDecimal price1,
+        BigDecimal size1,
+        BigDecimal price2,
+        BigDecimal size2,
+        String unit
+) {
+
+}
